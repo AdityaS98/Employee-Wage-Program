2bad0235678d8369c13922110b8c03be45c0d536
AdityaS98 <adityasharmalko1998@gmail.com>
2023-04-08 12:38:00 +0530

    Adding:Program to display Welcome message

---
+++ Employee_New.java
@@ -0,0 +1,22 @@
+package employee;
+/**
+ * 
+ * @author DELL
+ *
+ */
+
+public class Employee_New {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * Display message for welcoming into the program
+		 */
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+
+}
