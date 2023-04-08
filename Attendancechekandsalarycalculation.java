74397fb33c7bc6f09f8401e8985fc0c3a237b140
AdityaS98 <adityasharmalko1998@gmail.com>
2023-04-08 12:50:13 +0530

    Adding : Code for checking attendance

---
+++ Attendancechekandsalarycalculation.java
@@ -0,0 +1,26 @@
+package employee;
+/**
+ * 
+ * @author DELL
+ *
+ */
+
+public class Attendancechekandsalarycalculation {
+/**
+ * 
+ * @param args
+ */
+	public static void main(String[] args) {
+		/*
+		 * 1) A RANDOM function was created to check the attendance of employee
+		*/
+		double isPresent = Math.random();
+		if(isPresent>0.5) {
+			System.out.println("Employee is Present");
+		}
+		else
+		
+			System.out.println("Employee is Absent");
+	}
+
+}
